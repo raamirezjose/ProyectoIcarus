17ff633463bffe2446a07994324037ed35d7fdd7
lsmorales94 <lsmorales94@gmail.com>
2020-05-02 15:25:58 -0500

    botones

--- Movil/test_app/lib/main.dart
+++ Movil/test_app/lib/main.dart
@@ -1,9 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+
+import 'button.dart';
+import 'buttontapped.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,10 +35,98 @@ class Post {
 }
 
 class MyApp extends StatelessWidget {
-  get console => null;
-
   // This widget is the root of your application.
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+ 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  // SET ICONS HERE
+  var iconeast1 = Icons.arrow_right;
+  var iconnorth2 = Icons.arrow_drop_up;
+  var iconsouth3 = Icons.arrow_drop_down;
+  var iconwest4 = Icons.arrow_left;
+  var iconstop0 = Icons.stop;
+
+  bool btnPressEast1 = false;
+  bool btnPressNorth2 = false;
+  bool btnPressSouth3 = false;
+  bool btnPressWest4 = false;
+  bool btnPressStop0 = false;
+
+  void _letsPressStop0(){
+    print("stop");
+    stopPost();
+    setState(() {
+      btnPressStop0 = true;
+      btnPressEast1 = false;
+      btnPressNorth2 = false;
+      btnPressSouth3 = false;
+      btnPressWest4 = false;
+    });
+  }
+
+  void _letsPressEast1() {
+    print("east");
+    eastPost();
+    setState(() {
+      btnPressStop0 = false;
+      btnPressEast1 = true;
+      btnPressNorth2 = false;
+      btnPressSouth3 = false;
+      btnPressWest4 = false;
+    });
+  }
+
+  void _letsPressNorth2() {
+    print("north");
+    northPost();
+    setState(() {
+      btnPressStop0 = false;
+      btnPressEast1 = false;
+      btnPressNorth2 = true;
+      btnPressSouth3 = false;
+      btnPressWest4 = false;
+    });
+  }
+
+  void _letsPressSouth3() {
+    print("south");
+    southPost();
+    setState(() {
+      btnPressStop0 = false;
+      btnPressEast1 = false;
+      btnPressNorth2 = false;
+      btnPressSouth3 = true;
+      btnPressWest4 = false;
+    });
+  }
+
+  void _letsPressWest4() {
+    print("west");
+    westPost();
+    setState(() {
+      btnPressStop0 = false;
+      btnPressEast1 = false;
+      btnPressNorth2 = false;
+      btnPressSouth3 = false;
+      btnPressWest4 = true;
+    });
+  }
+
+@override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Routes',
@@ -54,21 +144,21 @@ class MyApp extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                getButton("North", 'images/flecha-arr.png' ,pAccion: () => [northPost()])
+                getButton(btnPressNorth2, iconnorth2 ,pAccion: () => [_letsPressNorth2()])
                 ],
               ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                getButton("West", 'images/flecha-izq.png' ,pAccion: () => [westPost()]),
-                getButton("Stop", 'images/stop.png' ,pAccion: () => [stopPost()]),
-                getButton("East", 'images/flecha-derecha.png' ,pAccion: () => [eastPost()]) 
+                getButton(btnPressWest4, iconwest4 ,pAccion: () => [_letsPressWest4()]),
+                getButton(btnPressStop0, iconstop0 ,pAccion: () => [_letsPressStop0()]),
+                getButton(btnPressEast1, iconeast1 ,pAccion: () => [_letsPressEast1()]) 
                 ], 
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                getButton("South", 'images/flecha-aba.png' ,pAccion: () => [southPost()])
+                getButton(btnPressSouth3, iconsouth3 ,pAccion: () => [_letsPressSouth3()])
               ],
             )
           ],
@@ -76,6 +166,24 @@ class MyApp extends StatelessWidget {
       )),
     );
   }
+
+
+  Expanded getButton(bool buttonPressed, IconData icon, {Function pAccion}) {  
+
+      var oBoton =  Expanded(
+                    child: GestureDetector(
+                        // FOURTH BUTTON
+                        onTap: pAccion,
+                        child: buttonPressed
+                            ? ButtonTapped(
+                                icon: icon,
+                              )
+                            : MyButton(
+                                icon: icon,
+                              )),
+                  );
+      return oBoton;
+  }  
 
   southPost() async {
     String url = 'http://10.0.2.2:3300/postroutemap';
@@ -144,28 +252,4 @@ class MyApp extends StatelessWidget {
     httpClient.close();
     return reply;
   }
-
-  MaterialButton getButton(String pTexto, String pathImage, {Function pAccion}) {      
-      var oBoton = MaterialButton(
-        textColor: Colors.white,
-        splashColor: Colors.greenAccent,
-        elevation: 8.0, 
-        child: Container( 
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(pathImage),
-              fit: BoxFit.fill),
-          ),
-          child: Padding(
-              padding: const EdgeInsets.all(40.0), 
-          ),
-        ),
-        onPressed: () {
-          pAccion();
-          print(pTexto);
-        },
-      );
-
-      return oBoton;
-  }  
 }
